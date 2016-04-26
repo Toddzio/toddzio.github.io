1f7198d4f026dfbd27cc0944c373c538f95bbc3d
Todd Kenna <i.toddicus@gmail.com>
2016-04-26 18:14:02 -0500

    Making changes available

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,11 +1,17 @@
-$( document ).ready(function() {
-	$( document.body ).one("click",function() {
+$(document).ready(function() {
+	$(document.body).one("click",function() {
 	  $('#photo').animate({
 	  	right: 400
 	  }, 3500, function(){});
 	  $( ".hidden" ).css('visibility','visible').hide().fadeIn(7000);
 
 	});
+	$('.slider').slick({
+    arrows: true,
+    lazyLoad: 'ondemand'
+  });
+
+// $('.single-item').slick();
 });
 
 
