4bd39ec65e37ff0a79abfb0c7cedcda5fc683407
Britney Jo <me@britneyjo.com>
2016-05-02 12:59:53 -0500

    fixed slick slider issue, made some css adjustments, corrected broken html

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
+
 	$(document.body).one("click",function() {
-	  $('#photo').animate({
+	  $('#headshot').animate({
 	  	left: '-200%'
 	  }, 7000, function(){});
 	  $(".hidden").fadeOut("slow", function() {
-    $(this).removeClass("loader");
-	});
+      $(this).removeClass("loader");
+	  });
 	  $( ".hidden" ).css('visibility','visible').hide().fadeIn(7000);
-
 	});
+
 	$('.slider').slick({
-    arrows: true,
-    lazyLoad: 'ondemand'
+
   });
 
 // $('.single-item').slick();
